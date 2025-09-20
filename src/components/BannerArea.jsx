300204cc14568c66e00d3e2a8b9a22b84ff15240
Kristina <kris07012000@gmail.com>
2025-09-20 22:48:36 +0300

    .

--- src/components/BannerArea.jsx
+++ src/components/BannerArea.jsx
@@ -2,10 +2,10 @@
 function BannerArea() {
   return (
     <section className="bannerArea" >
-			<p style={{fontSize:"4.5rem",  margin:"10rem 0rem 0rem 6.25rem", color:"#ffffff", fontFamily: "Halvar Breitschrift", fontWeight:"bold"}}>
+			<p className="bannerText1">
 				220FS + 100BYN
 			</p>
-			<p style={{fontSize:"1.5rem",  margin:"0.625rem 0rem 0rem 6.25rem", color:"#ffffff99", fontFamily: "Halvar Breitschrift", fontWeight:"bold" }}>
+			<p className="bannerText2">
 				ПРИВЕТСТВЕННЫЙ БОНУС ДЛЯ НОВИЧКОВ
 			</p>
 		</section>
